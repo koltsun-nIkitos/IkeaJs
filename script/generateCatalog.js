496ac63768426f95631700a13af4855903100377
Koltsun Nikita <koltsun-nv@yandex.ru>
2023-08-27 22:12:17 +0300

    v1.1.0

---
+++ script/generateCatalog.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const createCatalog = () =>{
+    const catalog = `
+    <div class="catalog">
+        <button type="button" class="btn btn-close catalog-btn" id="hnf-menu-close-btn" aria-expanded="true"
+                title="Закрыть меню" aria-label="Закрыть меню">
+            <svg focusable="false" class="svg-icon" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                    d="M12.0002 13.4144L16.9499 18.3642L18.3642 16.9499L13.4144 12.0002L18.3642 7.05044L16.95 5.63623L12.0002 10.586L7.05044 5.63623L5.63623 7.05044L10.586 12.0002L5.63624 16.9499L7.05046 18.3642L12.0002 13.4144Z"></path>
+            </svg>
+        </button>
+        <h2>Каталог</h2>
+        <ul class="catalog-list">
+            <li class="catalog-list__item active"><a href="goods.html?cat=Мебель">Мебель</a></li>
+            <li class="catalog-list__item"><a href="goods.html?cat=Кухня">Кухня</a></li>
+            <li class="catalog-list__item"><a href="goods.html?cat=Текстиль">Текстиль</a></li>
+            <li class="catalog-list__item"><a href="goods.html?cat=Освещение">Освещение</a></li>
+            <li class="catalog-list__item"><a href="goods.html?cat=Декор">Декор</a></li>
+        </ul>
+    </div>
+    `;
+
+    document.body.insertAdjacentHTML('beforeend', catalog);
+};
+
+createCatalog();
--- script/main.js
+++ script/main.js
@@ -2,10 +2,14 @@
 
 const btnBurger = document.querySelector(".btn-burger");
 const catalog = document.querySelector(".catalog");
-const overlay = document.querySelector(".overlay");
 const btnClose = document.getElementById("hnf-menu-close-btn");
 const subCatalog = document.querySelector(".subcatalog");
 const subcatalogHeader = document.querySelector(".subcatalog-header");
+const btnReturn = document.querySelector(".btn-return");
+const overlay = document.createElement('div');
+
+overlay.classList.add('overlay');
+document.body.insertAdjacentElement('beforeend', overlay);
 
 const openMenu = () =>{
     catalog.classList.add('open');
@@ -15,6 +19,7 @@ const openMenu = () =>{
 const closeMenu = () =>{
     catalog.classList.remove('open');
     overlay.classList.remove('active');
+    closeSubMenu();
 };
 
 
@@ -22,15 +27,21 @@ const openSubMenu = (event) =>{
     event.preventDefault();
     const itemList = event.target.closest('.catalog-list__item');
     if (itemList){
+        subcatalogHeader.innerHTML = itemList.innerHTML;
         subCatalog.classList.add('subopen');
-    }
-
+    };
 };
+
+const closeSubMenu = () =>{
+    subCatalog.classList.remove('subopen');
+};
+
 
 btnBurger.addEventListener("click", openMenu);
 btnClose.addEventListener("click", closeMenu);
 overlay.addEventListener("click", closeMenu);
 catalog.addEventListener("click", openSubMenu);
+btnReturn.addEventListener("click", closeSubMenu);
 
 
 document.addEventListener("keydown", (event) =>{
